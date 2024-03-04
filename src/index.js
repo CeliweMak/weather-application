2b30476b70dd841764dd0fcd03319f67ba754f78
CeliweMak <celiwemakgato@gmail.com>
2024-03-04 14:38:13 +0200

    Forecast API integration

--- src/index.js
+++ src/index.js
@@ -55,6 +55,11 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+}
+
 function getForecast(city) {
   let apiKey = "1ab0t30fc94fc0d3o78b8456ca58fd00";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -62,24 +67,31 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
      <div class="weather-forecast-day">
       <div class="weather-forecast-date>${day}</div>
-      <div class="weather-forecast-icon">⛅</div>
+      <div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+      </div>
       <div class="weather-forecast-temperature>
-       <span class="weather-forecast-temperature-max> 15°</span> 
-       <span class="weather-forecast-temperature-min> 12°</span>
+       <span class="weather-forecast-temperature-max>${Math.round(
+         day.temperature.maximum
+       )}°</span> 
+       <span class="weather-forecast-temperature-min>${Math.round(
+         day.temperature.minimum
+       )}°</span>
       </div>
     </div>   
 `;
+    }
   });
+
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
